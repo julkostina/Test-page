eb165cc146197bf864abce3d93b127ff2a1a497c
julkostina <julikostina1504@gmail.com>
2025-01-31 17:05:05 +0200

    slider  pagination buttons fixes

--- src/components/List/list.tsx
+++ src/components/List/list.tsx
@@ -3,7 +3,7 @@ import "./list.scss";
 import "swiper/css";
 import "swiper/css/pagination";
 import Search from "../Search/search.tsx";
-import TypeButton, {typeStyles} from "../TypeButton/typeButton.tsx";
+import TypeButton, { typeStyles } from "../TypeButton/typeButton.tsx";
 import OccupationButton from "../OccupationButton/occupationButton.tsx";
 import {
   InstDesign,
@@ -13,22 +13,24 @@ import {
 } from "../Icons/icons.tsx";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/pagination";
-
-import { Pagination } from "swiper/modules";
+import SwiperCore from "swiper";
+import { Pagination, Navigation } from "swiper/modules";
 import { ThemeContext } from "../../contexts/themeContext.tsx";
 
 const List: React.FC = () => {
   const [search, setSearch] = React.useState<string>("");
   const [type, setType] = React.useState<string[]>([]);
   const [occupation, setOccupation] = React.useState<string>("All");
-  const isDark = React.useContext(ThemeContext);
-  
+  const [sliderContent, setSliderContent] = React.useState<SwiperCore | null>(
+    null
+  );
+  const { isDark } = React.useContext(ThemeContext);
   type ItemDescription = {
     name: string;
     description: string;
     img: React.ReactElement<React.SVGProps<SVGElement>>;
   };
-  
+
   let itemDescriptions: ItemDescription[] = [
     {
       name: "INSTRUCTIONAL DESIGN",
@@ -75,48 +77,88 @@ const List: React.FC = () => {
       ),
     },
   ];
-  
   const arrayOfItems = typeStyles.slice(1).map((style) => {
-   return  itemDescriptions.flatMap((item) => {
+    return itemDescriptions.flatMap((item) => {
       return {
         name: item.name,
         description: item.description,
-        img: React.cloneElement(item.img, { fill: style.type[0], stroke: style.border?style.type[1]:"rgb(255, 255, 255)"}),
-        hrStyle: style.background!=="rgba(217, 217, 217, 0)"?style.background:style.type[1]
+        img: React.cloneElement(item.img, {
+          fill: style.type[0],
+          stroke: style.border ? style.type[1] : "rgb(255, 255, 255)",
+        }),
+        hrStyle:
+          style.background !== "rgba(217, 217, 217, 0)"
+            ? style.background
+            : style.type[1],
       };
     });
   });
-  const [arrayOfOccupations, setArrayOfOccupations] = React.useState<{ name: string; description: string; img: React.ReactNode; hrStyle: string; }[]>(arrayOfItems.flatMap((item)=>item));
-  console.log("type: "+type[0]);
-  React.useEffect(()=>{
-    if(occupation.toLocaleLowerCase()==="all"&&type.length===0){setArrayOfOccupations(()=>arrayOfItems.flat())}
-    if(occupation.toLocaleLowerCase()!=="all"&&type.length===0){setArrayOfOccupations(()=>arrayOfItems.flatMap(occupationArray => {
-      return occupationArray.filter(occ =>  occupation===occ.name.toLowerCase());
-    }));}
-    if(occupation.toLocaleLowerCase()==="all"&&type.length!==0){setArrayOfOccupations(()=>arrayOfItems.flatMap(occupationArray => {
-      return occupationArray.filter(occ =>  React.isValidElement(occ.img) && (occ.img.props as React.SVGProps<SVGElement>).fill === type[0]);
-    }));}
-    if(occupation.toLocaleLowerCase()!=="all"&&type.length!==0){setArrayOfOccupations(()=>arrayOfItems.flatMap(occupationArray => {
-      return occupationArray.filter(occ =>  occupation===occ.name.toLowerCase()&&React.isValidElement(occ.img) && (occ.img.props as React.SVGProps<SVGElement>).fill === type[0]);
-    }));}
-  },[occupation, type])
-  console.log("arrayOfOccupations: ",arrayOfOccupations);
-  const pagination = {
+  const [arrayOfOccupations, setArrayOfOccupations] = React.useState<
+    {
+      name: string;
+      description: string;
+      img: React.ReactNode;
+      hrStyle: string;
+    }[]
+  >(arrayOfItems.flatMap((item) => item));
+  console.log("type: " + type[0]);
+  React.useEffect(() => {
+    if (occupation.toLocaleLowerCase() === "all" && type.length === 0) {
+      setArrayOfOccupations(() => arrayOfItems.flat());
+    }
+    if (occupation.toLocaleLowerCase() !== "all" && type.length === 0) {
+      setArrayOfOccupations(() =>
+        arrayOfItems.flatMap((occupationArray) => {
+          return occupationArray.filter(
+            (occ) => occupation === occ.name.toLowerCase()
+          );
+        })
+      );
+    }
+    if (occupation.toLocaleLowerCase() === "all" && type.length !== 0) {
+      setArrayOfOccupations(() =>
+        arrayOfItems.flatMap((occupationArray) => {
+          return occupationArray.filter(
+            (occ) =>
+              React.isValidElement(occ.img) &&
+              (occ.img.props as React.SVGProps<SVGElement>).fill === type[0]
+          );
+        })
+      );
+    }
+    if (occupation.toLocaleLowerCase() !== "all" && type.length !== 0) {
+      setArrayOfOccupations(() =>
+        arrayOfItems.flatMap((occupationArray) => {
+          return occupationArray.filter(
+            (occ) =>
+              occupation === occ.name.toLowerCase() &&
+              React.isValidElement(occ.img) &&
+              (occ.img.props as React.SVGProps<SVGElement>).fill === type[0]
+          );
+        })
+      );
+    }
+  }, [occupation, type]);
+
+  let pagination = {
     clickable: true,
     renderBullet: function (index, className) {
-      let res='';
-      // if(index===0&&arrayOfOccupations.length>4){
-      //  res+='<button className="pagination-button"><img src="./imgs/arrow-grey.svg"/></button>';
-      // }
-      // if(index%4===0&&index!==arrayOfOccupations.length-1){
-        res+='<span class="' + className + `">` + (index + 1) + "</span>";
-      // }
-      // if(index===16&&arrayOfOccupations.length>4){
-      //   res+='<button className="pagination-button"><img src="./imgs/arrow-grey.svg"/></button>';
-      // }
+      let res = "";
+      if(index<3)
+      res += `<span class="${className} ${isDark ? "isDark" : ""}">${index + 1}</span>`;
       return res;
-    }
+    },
   };
+  React.useEffect(() => {
+    pagination = {
+      clickable: true,
+      renderBullet: function (index, className) {
+        let res = "";
+        res += `<span class="${className} ${isDark ? "isDark" : ""}">${index + 1}</span>`;
+        return res;
+      },
+    };
+  }, [isDark]);
   return (
     <div className="list">
       <div className="list-top">
@@ -129,33 +171,41 @@ const List: React.FC = () => {
       </div>
       <div className="list-description">
         <Swiper
-          pagination={pagination}
-          modules={[Pagination]}
+          modules={[Pagination, Navigation]}
+          onSwiper={setSliderContent}
           className="list-description-swiper"
           slidesPerView={4}
+          slidesPerGroup={4}
           spaceBetween={75}
-        >
-{arrayOfOccupations.map((item, index) => {
-  return (
-    <SwiperSlide key={index} style={{ display: "flex", gap: "75px" }}>
-      <div key={index}>
-        {item.img}
-        <h3 style={{ paddingTop: "30px", fontSize: "14px" }}>
-          {item.name}
-        </h3>
-        <hr
-          style={{
-            border: "none",
-            height: "3px",
-            backgroundColor: item.hrStyle,
+          pagination={pagination}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
           }}
-        />
-        <p>{item.description}</p>
-      </div>
-    </SwiperSlide>
-  );
-})}
-    
+        >
+          {arrayOfOccupations.map((item, index) => {
+            return (
+              <SwiperSlide>
+                <div key={index}>
+                  {item.img}
+                  <h3 style={{ paddingTop: "30px", fontSize: "14px" }}>
+                    {item.name}
+                  </h3>
+                  <hr
+                    style={{
+                      border: "none",
+                      height: "3px",
+                      backgroundColor: item.hrStyle,
+                    }}
+                  />
+                  <p>{item.description}</p>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+          
+          <div className={`swiper-button-prev ${isDark?"isDarkButton":""}`} ></div>
+          <div className={`swiper-button-next ${isDark?"isDarkButton":""}`}></div>
         </Swiper>
       </div>
     </div>
