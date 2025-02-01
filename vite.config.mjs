e5c273a138bfeb9ad9846477ca281d986fbaedde
Alex Trukhin <141617261+alextrukhin@users.noreply.github.com>
2025-02-01 19:48:10 +0100

    add base

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import viteTsconfigPaths from 'vite-tsconfig-paths'; 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()], 
-  build: {
-    commonjsOptions: { transformMixedEsModules: true },
-  },
-
+	plugins: [react(), viteTsconfigPaths()],
+	build: {
+		commonjsOptions: { transformMixedEsModules: true },
+	},
+	base: "/Test-page/dist/",
 });
