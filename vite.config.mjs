465a78aef280312d6a88f193e0893de350cc48dc
Alex Trukhin <141617261+alextrukhin@users.noreply.github.com>
2025-02-01 19:44:12 +0100

    Revert "Update vite.config.mjs"
    This reverts commit 65faee6106c78f9fb8e9771925532302111f07ca.

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 // Use dynamic import here:
-import viteTsconfigPaths from "vite-tsconfig-paths"; // Correct import
+import viteTsconfigPaths from 'vite-tsconfig-paths'; // Correct import
 
 export default defineConfig({
-	plugins: [react(), viteTsconfigPaths()], // Use the imported function
-
-	build: {
-		commonjsOptions: { transformMixedEsModules: true },
-	},
+  plugins: [react(), viteTsconfigPaths()], // Use the imported function
+  
 });
