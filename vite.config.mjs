65faee6106c78f9fb8e9771925532302111f07ca
Alex Trukhin <141617261+alextrukhin@users.noreply.github.com>
2025-02-01 19:41:21 +0100

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 // Use dynamic import here:
-import viteTsconfigPaths from 'vite-tsconfig-paths'; // Correct import
+import viteTsconfigPaths from "vite-tsconfig-paths"; // Correct import
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()], // Use the imported function
-  
+	plugins: [react(), viteTsconfigPaths()], // Use the imported function
+
+	build: {
+		commonjsOptions: { transformMixedEsModules: true },
+	},
 });
